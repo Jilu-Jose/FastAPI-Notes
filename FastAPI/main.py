9dc960a1f2e53939ff6acbbdf27a1b88c69f546a
Jilu P Jose <jilupjose111@gmail.com>
2026-04-21 09:17:44 +0530

    Day -1: FastAPI Basics

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+# 1) GET Method
 @app.get("/")
 def home():
     return {"message": "Hello FastAPI"}
@@ -10,6 +12,23 @@ def home():
 def about():
     return {"message": "This is not a DRill"}
 
+# 2) Path Parameters
 @app.get("/user/{user_id}")
 def get_user(user_id: int):
     return {"user_id": user_id}
+
+# 3) Query Parameters
+@app.get("/items/")
+def get_items(skip: int = 0, limit: int = 10):
+    return {"skip": skip, "limit":limit}
+
+# 4) POST MEthod
+class User(BaseModel):
+    name: str
+    age: int
+
+@app.post("/create_user/")
+def create_user(user: User):
+    return {
+        "user": user
+    }
